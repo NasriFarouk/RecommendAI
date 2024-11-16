ac950183c67cd684f28aa5e15e9698b37275cbc5
fnasri <165153470+NasriFarouk@users.noreply.github.com>
2024-11-16 21:02:43 +0100

    first commit

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -11,26 +11,35 @@ export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
+      tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+      headerShown: false,
       }}>
       <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
-          ),
-        }}
+      name="index"
+      options={{
+        title: 'Home',
+        tabBarIcon: ({ color, focused }) => (
+        <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
+        ),
+      }}
       />
       <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
-          ),
-        }}
+      name="explore"
+      options={{
+        title: 'Explore',
+        tabBarIcon: ({ color, focused }) => (
+        <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
+        ),
+      }}
+      />
+      <Tabs.Screen
+      name="anime"
+      options={{
+        title: 'Anime',
+        tabBarIcon: ({ color, focused }) => (
+        <TabBarIcon name={focused ? 'film' : 'film-outline'} color={color} />
+        ),
+      }}
       />
     </Tabs>
   );
